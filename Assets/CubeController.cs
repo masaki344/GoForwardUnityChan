b3549a9a4b19d068411c77c7caf378c36b2d6dfd
masaki344 <masakimiyoshi18.v.v@icloud.com>
2020-02-19 01:48:02 +0900

    GoForwardUnityChanプロジェクト　２Dゲーム作成
    課題の提出

---
+++ Assets/CubeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    //キューブの接触時に効果音をつけましょう
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //Unityちゃん以外に当たったら、効果音が鳴る
+        if (other.gameObject.name != "UnityChan2D")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+           
+}
